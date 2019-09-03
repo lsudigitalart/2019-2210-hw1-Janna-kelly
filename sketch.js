0e3aeb2916bd120db726af3300bb3a4de0caef0a
Janna Kelly <jannakelly@Jannas-MacBook-Air.local>
2019-09-03 12:27:08 -0500

    more

---
+++ sketch.js
@@ -0,0 +1,190 @@
+function setup() { 
+   createCanvas(800, 800);
+   //background(100);
+   fill(0);
+    ellipse(395, 395, 790, 787);
+    fill(255)
+    ellipse(395, 395, 690, 690);
+    fill(0)
+    ellipse(395, 395, 670, 670)
+    fill(255) ;
+    ellipse(395, 395, 120, 120);
+    fill(0)
+    ellipse(395, 395, 90, 90);
+    fill(255)
+    ellipse(395, 395, 60, 60);
+    fill(0)
+    ellipse(395, 395, 30, 30);
+    fill(255)
+    ellipse(450, 300, 100, 100);
+    fill(255)
+    ellipse(340, 300, 100, 100);
+    fill(255)
+    ellipse(450, 490, 100, 100);
+    fill(255)
+    ellipse(340, 490, 100, 100);
+    fill(255)
+    ellipse(535, 505, 65, 65);
+    fill(255)
+    ellipse(535, 285, 65, 65);
+    fill(255)
+    ellipse(593, 490, 50, 50);
+    fill(255)
+    ellipse(593, 300, 50, 50);
+    fill(255)
+    ellipse(634, 318, 35, 35);
+    fill(255)
+    ellipse(634, 472, 35, 35);
+    fill(255)
+    ellipse(656, 454, 23, 23);
+    fill(255)
+    ellipse(656, 337, 23, 23);
+    fill(255)
+    ellipse(663, 354, 15, 15);
+    fill(255)
+    ellipse(663, 437, 15, 15);
+    fill(255)
+    ellipse(256, 505, 65, 65);
+    fill(255)
+    ellipse(256, 285, 65, 65);
+    fill(255)
+    ellipse(198, 490, 50, 50);
+    fill(255)
+    ellipse(198, 300, 50, 50);
+    fill(255)
+    ellipse(157, 318, 35, 35);
+    fill(255)
+    ellipse(157, 472, 35, 35);
+    fill(255)
+    ellipse(135, 454, 23, 23);
+    fill(255)
+    ellipse(135, 337, 23, 23);
+    fill(255)
+    ellipse(126, 353, 15, 15);
+    fill(255)
+    ellipse(126, 437, 15, 15);
+    fill(255) ;
+    ellipse(535, 395, 130, 130);
+    fill(0) ;
+    ellipse(552, 395, 120, 120);
+    fill(255) ;
+    ellipse(570, 395, 110, 110);
+    fill(0) ;
+    ellipse(590, 395, 100, 100);
+    fill(255) ;
+    ellipse(605, 395, 90, 90);
+    fill(0) ;
+    ellipse(625, 395, 80, 80);
+    fill(255) ;
+    ellipse(635, 395, 70, 70);
+    fill(255)
+    ellipse(395, 239, 50, 50);
+    fill(255) ;
+    ellipse(255, 395, 130, 130);
+    fill(0) ;
+    ellipse(238, 395, 120, 120);
+    fill(255) ;
+    ellipse(220, 395, 110, 110);
+    fill(0) ;
+    ellipse(200, 395, 100, 100);
+    fill(255) ;
+    ellipse(186, 395, 90, 90);
+    fill(0) ;
+    ellipse(166, 395, 80, 80);
+    fill(255) ;
+    ellipse(155, 395, 70, 70);
+    fill(255)
+    ellipse(355, 215, 45, 45);
+    fill(255)
+    ellipse(437, 215, 45, 45);
+    fill(255)
+    ellipse(342, 178, 35, 35);
+    fill(255)
+    ellipse(445, 176, 35, 35);
+    fill(255)
+    ellipse(340, 145, 30, 30);
+    fill(255)
+    ellipse(445, 143, 30, 30);
+    fill(255)
+    ellipse(339, 118, 25, 25);
+    fill(255)
+    ellipse(445, 117, 25, 25);
+    fill(255)
+    ellipse(395, 550, 50, 50);
+    fill(255)
+    ellipse(355, 579, 45, 45);
+    fill(255)
+    ellipse(434, 579, 45, 45);
+    fill(255)
+    ellipse(343, 619, 35, 35);
+    fill(255)
+    ellipse(444, 619, 35, 35);
+    fill(255)
+    ellipse(342, 651, 30, 30);
+    fill(255)
+    ellipse(447, 651, 30, 30);
+    fill(255)
+    ellipse(342, 679, 25, 25);
+    fill(255)
+    ellipse(447, 679, 25, 25);
+    fill(255)
+    ellipse(535, 610, 100, 100);
+    fill(0)
+    ellipse(535, 610, 85, 85);
+    fill(255)
+    ellipse(255, 610, 100, 100);
+    fill(0)
+    ellipse(255, 610, 85, 85);
+    fill(255)
+    ellipse(535, 180, 100, 100);
+    fill(0)
+    ellipse(535, 180, 85, 85);
+    fill(255)
+    ellipse(255, 180, 100, 100);
+    fill(0)
+    ellipse(255, 180, 85, 85);
+    fill(255)
+    ellipse(628, 565, 70, 70);
+    fill(0)
+    ellipse(628, 565, 55, 55);
+    fill(255)
+    ellipse(165, 565, 70, 70);
+    fill(0)
+    ellipse(165, 565, 55, 55);
+    fill(255)
+    ellipse(165, 220, 70, 70);
+    fill(0)
+    ellipse(165, 220, 55, 55);
+    fill(255)
+    ellipse(628, 220, 70, 70);
+    fill(0)
+    ellipse(628, 220, 55, 55);
+    fill(255)
+    ellipse(675, 278, 50, 50);
+    fill(0)
+    ellipse(675, 278, 35, 35);
+    fill(255)
+    ellipse(675, 509, 50, 50);
+    fill(0)
+    ellipse(675, 509, 35, 35);
+    fill(255)
+    ellipse(125, 278, 50, 50);
+    fill(0)
+    ellipse(125, 278, 35, 35);
+    fill(255)
+    ellipse(125, 509, 50, 50);
+    fill(0)
+    ellipse(125, 509, 35, 35);
+    fill(255)
+    ellipse(91, 395, 50, 50);
+    fill(0)
+    ellipse(91, 395, 35, 35);
+    fill(255)
+    ellipse(698, 395, 50, 50);
+    fill(0)
+    ellipse(698, 395, 35, 35);
+}
+
+function draw() { 
+
+}
